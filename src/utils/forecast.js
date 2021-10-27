8730ad3bd0d1ba8d2909d90be99b52a3b0ef8064
unknown <dominiquedelmundo@outlook.com>
2021-10-27 15:22:41 +0800

    Added forecast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (longtitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+            const message =  body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. '
+                    + 'Wind speed is at ' + body.current.wind_speed + ' and wind degree is ' + body.current.wind_degree + '.'
+            callback(undefined, message)
         }
     })
 }
